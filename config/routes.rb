5ec2f02dbe2adf76845d5610e0c51a933dcb39b2
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-05-22 23:43:56 -0300

    configuração do caminho root e pagina de inicio para welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'welcome#index' #welcome nome do controller index a pagina para qual vai
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
