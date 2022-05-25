a4ac0dd0bc86696f6a4c43c9d0eb1f6156eb4d03
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-05-25 19:43:49 -0300

    metodo de auth do omniath-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :timeoutable
+         :timeoutable, :omniauthable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
